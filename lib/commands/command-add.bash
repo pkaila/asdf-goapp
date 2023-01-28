596ba516c6ee380741af413f4c768ca0a89d5d3d
Pauli Kaila <pauli.kaila@iki.fi>
2023-01-28 11:16:34 +0200

    feat: Possibility to override module name when adding a plugin

--- lib/commands/command-add.bash
+++ lib/commands/command-add.bash
@@ -15,16 +15,34 @@ if [ "$#" -eq 1 ]; then
 elif [ "$#" -eq 2 ]; then
   plugin_name="$1"
   package_path="$2"
+elif [ "$#" -eq 3 ]; then
+  plugin_name="$1"
+  package_path="$2"
+  module_name="$3"
 else
-  echo "Usage: asdf goapp add [<plugin_name>] <package_path>"
-  echo "    plugin_name: The name you want to give for the plugin"
-  echo "    package_path: The path for the go package to install when installing with the plugin"
+  echo "Usage:"
+  echo "  asdf goapp add [<plugin_name>] <package_path>"
+  echo "  asdf goapp add <plugin_name> <package_path> <module_name>"
+  echo ""
+  echo "Arguments:"
+  echo "  plugin_name:  The name you want to give for the plugin, if not given the last"
+  echo "                part of the package_path will be used. The name is always"
+  echo "                prepended with 'goapp-'."
+  echo "  package_path: The path for the go package to install when installing with the"
+  echo "                plugin."
+  echo "  module_name:  Override the Go module name if the plugin does not use the"
+  echo "                correct one automatically."
   exit 1
 fi
 
 plugin_name="goapp-${plugin_name}"
-module_name="$(resolve_go_module "$package_path")"
+if test -n "$module_name"; then
+  module_name="$(resolve_go_module "$package_path")"
+fi
 
-echo "Adding Go package ${package_path} from module $module_name as ASDF plugin ${plugin_name}"
+echo "Adding a new goapp plugin:"
+echo "  Plugin name: $plugin_name"
+echo "  Go package path: $package_path"
+echo "  Go module name: $module_name"
 log "Plugins dir: $plugins_dir, plugin source dir: $plugin_source_dir"
 add_plugin "$plugins_dir" "$plugin_source_dir" "$plugin_name" "$package_path" "$module_name"
